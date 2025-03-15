5a2d7b5c3993ebc42074c0625d2db84cf2aba3ec
fauzihazim <fauzihazim28@gmail.com>
2025-03-15 19:33:01 +0700

    add: bootstrap on editObat.php

--- controller/obat/editObat.php
+++ controller/obat/editObat.php
@@ -44,19 +44,32 @@ $conn->close();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>Edit Obat</title>
+    <link rel="stylesheet" href="../../css/stylesheet.css">
 </head>
-<body>
+<body class="m-10">
+    <a href="../../obat.php" class="btn btn-primary">
+        <i class="bi bi-chevron-left"></i> Back
+    </a>
     <h2>Edit Obat</h2>
     <form action="editObat.php" method="post">
         <input type="hidden" name="idObat" value="<?php echo $obat['idObat']; ?>">
-        <label for="namaObat">Nama Obat:</label>
-        <input type="text" id="namaObat" name="namaObat" value="<?php echo $obat['namaObat']; ?>"><br><br>
-        <label for="stokObat">Stok Obat:</label>
-        <input type="number" id="stokObat" name="stokObat" value="<?php echo $obat['stokObat']; ?>"><br><br>
-        <label for="harga">Harga:</label>
-        <input type="number" step="0.01" id="harga" name="harga" value="<?php echo $obat['harga']; ?>"><br><br>
-        <input type="submit" value="Update">
+        <div class="input-group mb-3 start-50 translate-middle" style="width: 80%;">
+            <span class="input-group-text" for="namaObat">Nama Obat :</span>
+            <input type="text" class="form-control" id="namaObat" name="namaObat" value="<?php echo $obat['namaObat']; ?>" placeholder="namaObat" aria-label="namaObat" aria-describedby="basic-addon1">
+        </div>
+        <div class="input-group mb-3 start-50 translate-middle" style="width: 80%;">
+            <span class="input-group-text" for="stokObat">Stok Obat :</span>
+            <input type="number" class="form-control" id="stokObat" name="stokObat" value="<?php echo $obat['stokObat']; ?>" placeholder="stokObat" aria-label="stokObat" aria-describedby="basic-addon1">
+        </div>
+        <div class="input-group mb-3 start-50 translate-middle" style="width: 80%;">
+            <span class="input-group-text" for="harga">Berat Pasien :</span>
+            <input type="number" class="form-control" id="harga" name="harga" value="<?php echo $obat['harga']; ?>" placeholder="harga" aria-label="harga" aria-describedby="basic-addon1">
+        </div>
+        <input class="btn btn-primary" type="submit" value="Update">
     </form>
 </body>
 </html>
